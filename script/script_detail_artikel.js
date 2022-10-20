ec9b35fb94a1727245b0110c0bc72ff80259ef38
hafiihzafarhana <hafiihza2002@gmail.com>
2022-10-21 00:56:14 +0700

    Hafi-revisi halaman utama, tamah artikel, edit artikel

--- script/script_detail_artikel.js
+++ script/script_detail_artikel.js
@@ -2,6 +2,7 @@
 const isi_detail = document.getElementById('isi_detail');
 let edit_data;
 let tambahData;
+let hapus_data;
 
 // ===================================================== fetch ===============================================
 function getSlug(){
@@ -16,16 +17,24 @@ fetch(`https://634be8e9317dc96a308d3518.mockapi.io/ayf/artikel?slug=${getSlug()}
     .then(datas => {
         checkUserInWebStorage()
         let detail = datas[0];
-
-        let apakahDiaLike = detail.like_artikel.filter((e) => e.username_user == pengguna_saat_ini.username_user);
+        let a = detail.komentar.join(' ')
 
         let isi = `<p class="fw-bold text-center text-24px">${detail.judul_artikel}</p>
         <p class="text-12px">${detail.created_at}</p>
-        <div class="detail_artikel_nama_icon">
+        <div class="detail_artikel_nama_icon my-2">
             <p class="text-16px">${detail.nama_penulis}</p>
-            <div class="d-flex gap-3">
-                <button class="btn" onClick="edit_data('${detail.id_artikel}')" id="untukLike"><i class="fa-solid fa-thumbs-up"></i> <span id="jumlah_likenya">${detail.like_artikel.length}</span></button>
+            <div class="d-flex">
+                <button class="btn" onClick="edit_data('${detail.id_artikel}')" id="untukLike"><i class="fa-solid fa-thumbs-up"></i> <span id="jumlah_likenya">${detail.like_artikel.length}</span>
+                </button>
                 <button class="btn"><i class="fa-solid fa-share"></i></button>
+
+                <div class="btn-group d-flex align-items-center justify-content-center" id="dropdow_profile">
+                ${(pengguna_saat_ini.role == 2 ? (`<button type="button" class="btn dropdown-toggle-split" data-bs-toggle="dropdown"><i class="fa-solid fa-ellipsis-vertical"></i></button>`) : (``))}
+                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-start mt-2" id="drowdown_list">
+                        <li><a href="./edit_artikel.html?slug=${detail.slug}" class="dropdown-item btn">Ubah</a></li>
+                        <li><button onClick="hapus_data('${detail.id_artikel}')" class="dropdown-item btn">Hapus</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
         <img src="${detail.gambar_artikel}" class="img-me" alt="">
@@ -36,18 +45,16 @@ fetch(`https://634be8e9317dc96a308d3518.mockapi.io/ayf/artikel?slug=${getSlug()}
         </p>
         <div class="collapse mt-3" id="collapseExample">
             <div class="card card-body">
-                <div class="d-flex flex-column">
-                    ${detail.komentar.map((e) =>{
-                        return(`
+                <div class="d-flex flex-column" id="isiKomenanOrang">
+                    ${detail.komentar.map((e) =>(`
                         <div class="d-flex flex-column">
-                        <div class="d-flex gap-2">
-                            <p class="fw-bold">${e.username_user}</p>
-                            <p class="text-12px"><i>21 Desember 2022</i></p>
-                        </div>
-                        <p>"${e.isiKomen}"</p>
-                    </div>
-                    `)
-                    })}
+                            <div class="d-flex gap-2">
+                                <p class="fw-bold">${e.username_user}</p>
+                                <p class="text-12px"><i>21 Desember 2022</i></p>
+                            </div>
+                            <p>"${e.isiKomen}"</p>
+                        </div>`)
+                    )}
                 </div>
                 <div class="mt-3">
                     <div class="form-floating">
@@ -63,15 +70,19 @@ fetch(`https://634be8e9317dc96a308d3518.mockapi.io/ayf/artikel?slug=${getSlug()}
         let isi_isi = document.getElementById('isi_isi');
         isi_isi.innerText = detail.isi
 
-        if(apakahDiaLike.length > 0){
-            document.getElementById('untukLike').style.color="yellow"
-        } else{
-            document.getElementById('untukLike').style.color = "black"
+        if(pengguna_saat_ini != null){
+            let apakahDiaLike = detail.like_artikel.filter((e) => e.username_user == pengguna_saat_ini.username_user);
+            if(apakahDiaLike.length > 0){
+                document.getElementById('untukLike').style.color="yellow"
+            } else{
+                document.getElementById('untukLike').style.color = "black"
+            }
         }
     })
 
 
     edit_data = (id) => {
+        if(pengguna_saat_ini != null){
         fetch(`https://634be8e9317dc96a308d3518.mockapi.io/ayf/artikel?id_artikel=${id}`)
             .then(subjek => subjek.json())
             .then(dataArtikel => {
@@ -117,9 +128,13 @@ fetch(`https://634be8e9317dc96a308d3518.mockapi.io/ayf/artikel?slug=${getSlug()}
                     })
                 }
             })
+        } else{
+            window.location.href = "./login.html";
+        }
     }
 
     tambahData = (id) => {
+        if(pengguna_saat_ini != null){
         fetch(`https://634be8e9317dc96a308d3518.mockapi.io/ayf/artikel?id_artikel=${id}`)
             .then(subjek => subjek.json())
             .then(data => {
@@ -127,6 +142,7 @@ fetch(`https://634be8e9317dc96a308d3518.mockapi.io/ayf/artikel?slug=${getSlug()}
                     return e;
                 })
 
+                if(document.getElementById('fillKomen').value != ""){
                 let dataKomenBaru = {
                     id_komentar:data[0].komentar.length+1,
                     username_user:pengguna_saat_ini.username_user,
@@ -141,10 +157,39 @@ fetch(`https://634be8e9317dc96a308d3518.mockapi.io/ayf/artikel?slug=${getSlug()}
                     headers: {
                         "Content-Type": "application/json",
                     },
-                    body:JSON.stringify({
-                        komentar:dataMasuk
-                    })
+                    body:JSON.stringify({komentar:dataMasuk})
                 })
+                .then(subjek => subjek.json())
+                    .then(datas => {
+                        let isiKomenanOrang = document.getElementById('isiKomenanOrang');
+                        let isiSemuaKomentar = datas.komentar.map((e) => {
+                            return(`
+                            <div class="d-flex flex-column">
+                                <div class="d-flex gap-2">
+                                    <p class="fw-bold">${e.username_user}</p>
+                                    <p class="text-12px"><i>21 Desember 2022</i></p>
+                                </div>
+                                <p>"${e.isiKomen}"</p>
+                            </div>`)
+                        })
+
+                        isiKomenanOrang.innerHTML = isiSemuaKomentar;
+                        document.getElementById('fillKomen').value =""
+                    })
+                } else{
+                    alert("Komentar tidak boleh kosong")
+                }
             })
+        } else{
+            window.location.href = "./login.html";
+        }
     }
 
+    hapus_data = (id) => {
+        fetch(`https://634be8e9317dc96a308d3518.mockapi.io/ayf/artikel/${id}`,{
+            method:"DELETE"
+        })
+        .then(() => {
+            window.location.href="./../index.html";
+        })
+    }
