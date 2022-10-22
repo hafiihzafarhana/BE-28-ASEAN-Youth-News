b8786329fdaa8f0d05fabba2d70a74e9804e5312
hafiihzafarhana <hafiihza2002@gmail.com>
2022-10-22 15:40:45 +0700

    Hafi - Finish (1)

--- script/script_detail_artikel.js
+++ script/script_detail_artikel.js
@@ -1,5 +1,6 @@
 // ===================================================== Reference ===========================================
 const isi_detail = document.getElementById('isi_detail');
+const data_terkini_display = document.getElementById('data_terkini_display');
 let edit_data;
 let tambahData;
 let hapus_data;
@@ -78,6 +79,53 @@ fetch(`https://634be8e9317dc96a308d3518.mockapi.io/ayf/artikel?slug=${getSlug()}
                 document.getElementById('untukLike').style.color = "black"
             }
         }
+    })
+
+    fetch('https://634be8e9317dc96a308d3518.mockapi.io/ayf/artikel')
+        .then(subjek => subjek.json())
+        .then(data => {
+        const filterTanpa1 = data.filter((e) => e.id_artikel != 1)
+        const terpopuler = filterTanpa1.sort((a,b) => {
+            return b.komentar.length - a.komentar.length
+        })
+
+        // data populer
+        const arrayPopuler = terpopuler.slice(0,3)
+
+        let dataPalingTersisih = filterTanpa1.filter( x => !arrayPopuler.map(e => e.id_artikel).includes(x.id_artikel) );
+        const teratas = dataPalingTersisih.sort((a,b) => {
+            return b.like_artikel.length - a.like_artikel.length
+        })
+
+        // data teratas
+        const arrayTeratas = teratas.slice(0,3);
+
+        let dataPalingTersisihPart2 = filterTanpa1.filter( x => !arrayPopuler.map(e => e.id_artikel).includes(x.id_artikel) && !arrayTeratas.map(e => e.id_artikel).includes(x.id_artikel));
+
+        // dataPalingTersisihPart2
+        const terkini = dataPalingTersisihPart2.sort((a,b) => {
+            return b.id_artikel - a.id_artikel
+        })
+
+        const arrayTerkini = terkini.slice(0,3);
+
+        teks3 = "";
+        arrayTerkini.map((e) =>{
+        teks3 += `
+        <div class="col-12">
+                <a href="/public/detail_artikel.html?slug=${e.slug}">
+                  <div class="card" style="width: 100%;">
+                    <img src="${e.gambar_artikel}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">${e.judul_artikel.length >= 25 ? (e.judul_artikel.slice(0,25) + ' ...') : (e.judul_artikel)}</h5>
+                    </div>
+                  </div>
+                </a>
+              </div>
+        `;
+        })
+
+        data_terkini_display.innerHTML = teks3
     })
 
 
